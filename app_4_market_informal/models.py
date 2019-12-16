cd785d9c610325a08bbd26c2dd4b191755f48c0c
rafaelcharris <rafaelcharris92@gmail.com>
2019-12-16 17:01:04 -0500

    agregar row a report

--- app_4_market_informal/models.py
+++ app_4_market_informal/models.py
@@ -102,7 +102,7 @@ class Group(BaseGroup):
                     p.payoff = int(Constants.endowment)
             else:
                 p.payoff = int(Constants.endowment)
-                p.payoff += int(p.ask_price_fin - p.seller_valuation)*int(p.sold) - int(p.see_list)*int(Constants.see_list_cost)
+                p.payoff += (int(p.ask_price_fin) - int(p.seller_valuation))*int(p.sold) - int(p.see_list)*int(Constants.see_list_cost)
 
     def who_purchased(self):
         sellers =[]
