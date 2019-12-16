1ac9218010832e9bf6667e2e1250c80ff52ef189
Felipe Montealegre <43189041+felmola@users.noreply.github.com>
2019-12-16 18:26:59 -0500

    changed final payoff function 1.2 multiplier on compract app

--- app_1_market_com_practices/models.py
+++ app_1_market_com_practices/models.py
@@ -29,6 +29,7 @@ class Constants(BaseConstants):
     endowment = 30
     see_list_cost = 1
     reference_20 = 20
+    ecu_eur = 1.2
 
     packages = [i for i in range(1, 6)]
 
@@ -227,13 +228,16 @@ class Player(BasePlayer):
     time_spent = models.FloatField()
     paying_round = models.IntegerField()
     payoff_final = models.IntegerField()
+    payoff_euro = models.FloatField()
     discount = models.IntegerField()
 
     def payoff_final_f(self):
         self.paying_round = random.randint(1, Constants.num_rounds)
         self.payoff_final = int(self.in_round(self.paying_round).payoff)
+        self.payoff_euro = self.payoff_final * Constants.ecu_eur
         self.participant.vars['paying_round'] = self.paying_round
         self.participant.vars['payoff_final'] = self.payoff_final
+        self.participant.vars['payoff_euro'] = self.payoff_euro
         self.session.vars['endowment'] = Constants.endowment
         print("##########################", self.paying_round)
         print("##########################", self.payoff_final)
